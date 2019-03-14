2a41363f9a036f4918bee601aab616aa6cd09d3b
Justin C. Bagley <justincbagley@users.noreply.github.com>
2019-03-14 15:11:12 -0500

    Update piranha.rb

--- Formula/piranha.rb
+++ Formula/piranha.rb
@@ -14,7 +14,7 @@ class PIrANHA < Formula
   
   desc "Scripts for file processing and analysis in phylogenomics & phylogeography"
   homepage "https://justinbagley.org/PIrANHA/"
-  url "https://github.com/justincbagley/PIrANHA/archive/v0.1.7.tar.gz"
+  url "https://github.com/justincbagley/PIrANHA/archive/v0.2.tar.gz"
   sha256 "<SHA256>"
 
   ## Optionally, specify a repository to be used. Brew then generates a '--HEAD | --head' option. 
@@ -35,14 +35,34 @@ class PIrANHA < Formula
   depends_on :python
   depends_on :perl
 
-  ## Using 'bin.install' here is essentially saying move '<script>' into #{prefix}/bin/.
+  ## Using 'bin.install' here is essentially saying move '<script>' into #{prefix}/bin/. prefix.install is 
+  ## essentially saying move/save script into the main or #{prefix} dir. Dir helps us copy a whole directory
+  ## with ease.
   def install
-     bin.install "piranha"
+    prefix.install "piranha"
+    prefix.install "53.phy"
+    prefix.install "LICENSE"
+    prefix.install "README.md"
+    prefix.install "Subset91_concat.fas"
+    prefix.install "Subset92_concat.fas"
+    prefix.install "Subset93_concat.fas"
+    prefix.install "Subset94_concat.fas"
+    prefix.install "Subset95_concat.fas"
+    prefix.install "Subset96_concat.fas"
+    prefix.install "Subset97_concat.fas"
+    prefix.install "Subset98_concat.fas"
+    prefix.install "Subset99_concat.fas"
+    prefix.install "Subset9_concat.fas"
+    prefix.install Dir["bin"]    
+    prefix.install Dir["lib"]    
+    prefix.install Dir["etc"]    
+    prefix.install Dir["doc"]    
+    prefix.install Dir["tmp"]    
   end
 
   test do
     ## Modified example, commented out: assert_match "piranha v1.0", shell_output("#{bin}/piranha -V", 2)
-    system bin/"piranha", "--version"
+    system "piranha", "--version"
   end
 
 end
@@ -56,3 +76,4 @@ end
 ##
 ## After tapping, install is as easy as ```$ brew install piranha ```
 ##
+
